74c6b1709f3d2cd75e471189de357c6b3f6eab8f
skeleton1231 <814010079@qq.com>
2022-11-06 22:34:21 +0800

    feat:app;framework/cobra

---
+++ framework/cobra/hade_command.go
@@ -0,0 +1,14 @@
+package cobra
+
+import "github.com/skeleton1231/skeleton/framework"
+
+// SetContainer 设置服务容器
+
+func (c *Command) SetContainer(container framework.Container) {
+	c.container = container
+}
+
+// GetContainer 获取容器
+func (c *Command) GetContainer() framework.Container {
+	return c.Root().container
+}
--- main.go
+++ main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
-	"context"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
-	"github.com/skeleton1231/skeleton/framework/gin"
-	"github.com/skeleton1231/skeleton/framework/middleware"
-	"github.com/skeleton1231/skeleton/provider/demo"
+	"github.com/skeleton1231/skeleton/app/console"
+	"github.com/skeleton1231/skeleton/app/http"
+	"github.com/skeleton1231/skeleton/framework"
+	"github.com/skeleton1231/skeleton/framework/provider/app"
+	"github.com/skeleton1231/skeleton/framework/provider/kernel"
 )
 
 func main() {
+	// 初始化服务容器
+	container := framework.NewHadeContainer()
+	// 绑定App服务提供者
+	container.Bind(&app.HadeAppProvider{})
+	// 后续初始化需要绑定的服务提供者...
+
+	// 将HTTP引擎初始化,并且作为服务提供者绑定到服务容器中
+	if engine, err := http.NewHttpEngine(); err == nil {
+		container.Bind(&kernel.HadeKernelProvider{HttpEngine: engine})
+	}
+
+	// 运行root命令
+	console.RunCommand(container)
+}
+
+/*func main() {
 	//core := framework.NewCore()
 	core := gin.New()
 
 	//core.Bind(&app.HadeAppProvider{})
+	container := framework.NewHadeContainer()
+
+	// 绑定 App 服务提供者
+	container.Bind(&app.HadeAppProvider{})
+
+	// 后续初始化需要绑定的服务提供者...
+	// 将 HTTP 引擎初始化,并且作为服务提供者绑定到服务容器中
+	if engine, err := http.NewHttpEngine(); err == nil {
+		container.Bind(&kernel.HadeKernelProvider{HttpEngine: engine})
+	}
 
 	//bind
 	core.Bind(&demo.DemoServiceProvider{})
@@ -54,4 +74,4 @@ func main() {
 	if err := server.Shutdown(timeoutCtx); err != nil {
 		log.Fatal("Server Shutdown:", err)
 	}
-}
+}*/
